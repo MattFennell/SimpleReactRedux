e1f808a9a23e4bb85f1e1df2da13bb7b4eb1e9cc
MattFennell <mfennell@scottlogic.com>
2021-11-19 13:31:48 +0000

    Using local state only!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./slice";
 
 function App() {
-  const countValue = useSelector((state) => state.counter.value);
+  const [counter, setCounter] = useState(0);
 
-  const dispatch = useDispatch();
-
-  const incrementCounter = () => {
-    dispatch(increment());
-  };
-
-  const decrementCounter = () => {
-    dispatch(decrement());
-  };
+  const incrementCounter = () => setCounter(counter + 1);
+  const decrementCounter = () => setCounter(counter - 1);
 
   return (
     <div className="App" style={{ textAlign: "center", marginTop: "30px" }}>
       <button onClick={incrementCounter}> Increase </button>
       <button onClick={decrementCounter}> Decrease </button>
-      <div>{countValue}</div>
+      <div
+        style={{
+          marginTop: "30px",
+          fontSize: "40px",
+          color: "red",
+          fontWeight: "bold",
+        }}
+      >
+        {counter}
+      </div>
     </div>
   );
 }
