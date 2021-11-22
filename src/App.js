d384fe57234f932b9c8fd8c6f95c6002f3cb35d8
MattFennell <mfennell@scottlogic.com>
2021-11-22 12:04:22 +0000

    BLank app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./slice";
-
+import React from "react";
 function App() {
-  const countValue = useSelector((state) => state.counter.value);
-
-  const dispatch = useDispatch();
-
-  const incrementCounter = () => {
-    dispatch(increment());
-  };
-
-  const decrementCounter = () => {
-    dispatch(decrement());
-  };
-
   return (
     <div className="App" style={{ textAlign: "center", marginTop: "30px" }}>
-      <button onClick={incrementCounter}> Increase </button>
-      <button onClick={decrementCounter}> Decrease </button>
-      <div>{countValue}</div>
+      Hello
     </div>
   );
 }
